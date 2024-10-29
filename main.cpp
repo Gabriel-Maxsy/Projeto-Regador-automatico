a7c78a68f90227ceb992a39d83ea4e41dea7a7e2
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-10-29 00:07:44 -0300

    Código atualizado
    Projeto quase concluido, tudo montado e código atualizado, funcionando sem nenhum problema, ainda faltam alguns testes, quando concluidos os testes, irei atualizar o readme com fotos reais e disponibilizar videos.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,30 @@
-// C++ code
-//
-void setup()
-{
-  pinMode(7, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(1,OUTPUT);
+#define rele 3
+#define sensor 2
+
+bool irrigar = true;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(rele, OUTPUT);
+  pinMode(sensor, INPUT);
+
+  digitalWrite(rele, HIGH);
 }
 
-void loop()  
-{
-  int vermelho = 1;
-  int verde = 12;
-  int amarelo = 7;
-  digitalWrite(verde, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(verde, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(amarelo, HIGH);
-  delay(1000);
-  digitalWrite(amarelo, LOW);
-  delay(1000);
-  digitalWrite(vermelho, HIGH);
-  delay(1000);
-  digitalWrite(vermelho, LOW);
-  delay(1000);	
-  digitalWrite(LED_BUILTIN, LOW);
-}
+void loop() {
+  // put your main code here, to run repeatedly:
+  // irrigar = digitalRead(sensor);
 
-// led parte menor negativa, parte maior positiva
+  if(irrigar)
+  {
+    digitalWrite(rele, LOW);
+    delay(5000);
+    digitalWrite(rele, HIGH);
+    delay(3000);
+  }
+  else
+  {
+    digitalWrite(rele, HIGH);
+    delay(1000);
+  }
+  delay(500);
+}
